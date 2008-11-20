0675c5f0f8f201f904ee70338a4205d4ae50572a
Enrico Scholz <enrico.scholz@sigma-chemnitz.de>
2008-11-20 14:27:55 +0100

    use sentinel NULL in execl(2) call; increased size of /dev

--- init.wrapper.c
+++ init.wrapper.c
@@ -9,13 +9,13 @@
 
 int main()
 {
-  if (mount("none", "/dev", "tmpfs", 0, "size=256k")==-1 ||
+  if (mount("none", "/dev", "tmpfs", 0, "size=512k")==-1 ||
       mknod("/dev/console", 0600 | S_IFCHR, makedev(5,1))==-1 ||
       mknod("/dev/null",    0666 | S_IFCHR, makedev(1,3))==-1 ||
       (close(0),close(1),close(2), 0) ||
       open("/dev/console", O_RDONLY)==-1 ||
       open("/dev/console", O_WRONLY)==-1 ||
       dup2(1,2)==-1 ||
-      execl("/sbin/init.wrapped", "/sbin/init", 0)==-1)
+      execl("/sbin/init.wrapped", "/sbin/init", NULL)==-1)
     return EXIT_FAILURE;
 }
