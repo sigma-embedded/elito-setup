47a9750fd8cc5acce32b765b8f09b3d974a08e81
Enrico Scholz <enrico.scholz@sigma-chemnitz.de>
2009-11-04 17:20:32 +0100

    whitespace cleanups added license plate

--- redir-outerr.c
+++ redir-outerr.c
@@ -1,3 +1,19 @@
+/*	--*- c -*--
+ * Copyright (C) 2009 Enrico Scholz <enrico.scholz@sigma-chemnitz.de>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -7,36 +23,36 @@
 
 int main(int argc, char *argv[])
 {
-  int		fd;
-  if (argc<4) {
-    write(2, "Usage: redir-outerr <OUT> <ERR> <CMD> <PARAMS>*\n", 48);
-    return EXIT_FAILURE;
-  }
+	int		fd;
+	if (argc<4) {
+		write(2, "Usage: redir-outerr <OUT> <ERR> <CMD> <PARAMS>*\n", 48);
+		return EXIT_FAILURE;
+	}
 
-  fd = open(argv[1], O_WRONLY);
-  if (fd==-1) {
-    write(2, "redir-outerr: open(OUT) failed\n", 31);
-    return EXIT_FAILURE;
-  }
+	fd = open(argv[1], O_WRONLY);
+	if (fd==-1) {
+		write(2, "redir-outerr: open(OUT) failed\n", 31);
+		return EXIT_FAILURE;
+	}
 
-  if (fd!=1) {
-    dup2(fd, 1);
-    close(fd);
-  }
+	if (fd!=1) {
+		dup2(fd, 1);
+		close(fd);
+	}
 
 
-  fd = open(argv[2], O_WRONLY);
-  if (fd==-1) {
-    write(2, "redir-outerr: open(ERR) failed\n", 31);
-    return EXIT_FAILURE;
-  }
+	fd = open(argv[2], O_WRONLY);
+	if (fd==-1) {
+		write(2, "redir-outerr: open(ERR) failed\n", 31);
+		return EXIT_FAILURE;
+	}
 
-  if (fd!=2) {
-    dup2(fd, 2);
-    close(fd);
-  }
-  
-  execv(argv[3], argv+3);
-  write(2, "redir-outerr: execv() failed\n", 27);
-  return EXIT_FAILURE;
+	if (fd!=2) {
+		dup2(fd, 2);
+		close(fd);
+	}
+
+	execv(argv[3], argv+3);
+	write(2, "redir-outerr: execv() failed\n", 27);
+	return EXIT_FAILURE;
 }
--- sysctl.minit.c
+++ sysctl.minit.c
@@ -1,3 +1,19 @@
+/*	--*- c -*--
+ * Copyright (C) 2009 Enrico Scholz <enrico.scholz@sigma-chemnitz.de>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
@@ -8,36 +24,36 @@
 
 int main(int argc, char *argv[])
 {
-  int			i;
+	int			i;
 
-  if (argc%2 != 1) {
-    WRITE_MSG(2, "Bad parameter count\n");
-    return EXIT_FAILURE;
-  }
+	if (argc%2 != 1) {
+		WRITE_MSG(2, "Bad parameter count\n");
+		return EXIT_FAILURE;
+	}
 
-  for (i=1; i<argc; i+=2) {
-    int		fd      = open(argv[i], O_WRONLY);
-    char const *err_pos = 0;
-    if (fd==-1) err_pos = "open";
-    else if (write(fd, argv[i+1], strlen(argv[i+1]))==-1 ||
-	     write(fd, "\n", 1)!=1)
-      err_pos = "write";
+	for (i=1; i<argc; i+=2) {
+		int		fd      = open(argv[i], O_WRONLY);
+		char const *err_pos = 0;
+		if (fd==-1) err_pos = "open";
+		else if (write(fd, argv[i+1], strlen(argv[i+1]))==-1 ||
+			 write(fd, "\n", 1)!=1)
+			err_pos = "write";
 
 
-    if (err_pos!=0) {
-      size_t	l0 = strlen(err_pos);
-      size_t	l1 = strlen(argv[i]);
-      char	msg[l0+l1 + 3];
+		if (err_pos!=0) {
+			size_t	l0 = strlen(err_pos);
+			size_t	l1 = strlen(argv[i]);
+			char	msg[l0+l1 + 3];
 
-      strcpy(msg, err_pos);
-      strcat(msg, "(");
-      strcat(msg, argv[i]);
-      strcat(msg, ")");
+			strcpy(msg, err_pos);
+			strcat(msg, "(");
+			strcat(msg, argv[i]);
+			strcat(msg, ")");
 
-      perror(msg);
-    }
+			perror(msg);
+		}
 
-    if (fd!=-1)
-      close(fd);
-  }
+		if (fd!=-1)
+			close(fd);
+	}
 }
