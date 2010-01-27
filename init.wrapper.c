e09add53f9f88b4c08456466b2365c5f608b6f77
Enrico Scholz <enrico.scholz@sigma-chemnitz.de>
2010-01-27 16:42:38 +0100

    init.wrapper: detect devtmpfs by checking for /dev/null before mounting /dev

--- init.wrapper.c
+++ init.wrapper.c
@@ -9,11 +9,19 @@
 
 int main()
 {
-	if (mount("none", "/dev", "tmpfs", 0, "size=512k")==-1 ||
-	    mknod("/dev/console", 0600 | S_IFCHR, makedev(5,1))==-1 ||
-	    mknod("/dev/null",    0666 | S_IFCHR, makedev(1,3))==-1 ||
-	    (close(0),close(1),close(2), 0) ||
-	    open("/dev/console", O_RDONLY)==-1 ||
+	struct stat	st;
+
+	if (stat("/dev/null", &st) < 0 &&
+	    (mount("none", "/dev", "tmpfs", 0, "size=512k")==-1 ||
+	     mknod("/dev/console", 0600 | S_IFCHR, makedev(5,1))==-1 ||
+	     mknod("/dev/null",    0666 | S_IFCHR, makedev(1,3))==-1))
+		return EXIT_FAILURE;
+
+	close(0);
+	close(1);
+	close(2);
+
+	if (open("/dev/console", O_RDONLY)==-1 ||
 	    open("/dev/console", O_WRONLY)==-1 ||
 	    dup2(1,2)==-1 ||
 	    execl("/sbin/init.wrapped", "/sbin/init", NULL)==-1)
