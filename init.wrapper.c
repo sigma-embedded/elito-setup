a1226abbbe02664ecd1060f781c7e43bb4b8b690
Enrico Scholz <enrico.scholz@sigma-chemnitz.de>
2010-01-29 12:03:26 +0100

    use correct way to check for devtmpfs

--- init.wrapper.c
+++ init.wrapper.c
@@ -6,12 +6,17 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <sys/vfs.h>
+
+#ifndef TMPFS_MAGIC
+#  define TMPFS_MAGIC	0x01021994
+#endif
 
 int main()
 {
-	struct stat	st;
+	struct statfs	st;
 
-	if (stat("/dev/null", &st) < 0 &&
+	if ((statfs("/dev", &st) < 0 || st.f_type != TMPFS_MAGIC) &&
 	    (mount("none", "/dev", "tmpfs", 0, "size=512k")==-1 ||
 	     mknod("/dev/console", 0600 | S_IFCHR, makedev(5,1))==-1 ||
 	     mknod("/dev/null",    0666 | S_IFCHR, makedev(1,3))==-1))
@@ -21,9 +26,11 @@ int main()
 	close(1);
 	close(2);
 
-	if (open("/dev/console", O_RDONLY)==-1 ||
-	    open("/dev/console", O_WRONLY)==-1 ||
-	    dup2(1,2)==-1 ||
-	    execl("/sbin/init.wrapped", "/sbin/init", NULL)==-1)
+	if (open("/dev/console", O_RDONLY)!=0 ||
+	    open("/dev/console", O_WRONLY)!=1 ||
+	    dup2(1,2)==-1)
 		return EXIT_FAILURE;
+
+	execl("/sbin/init.wrapped", "/sbin/init", NULL);
+	return EXIT_FAILURE;
 }
